b7373e3665cb39334c257b3ce1c693b0da1b2331
Matthew Tse <tsemd@bu.edu>
2019-12-12 11:36:30 -0500

    cleaned up main game board

--- src/view/Game.java
+++ src/view/Game.java
@@ -15,47 +15,62 @@ import model.Player;
  *
  */
 public class Game {
-	
+	public static String playerOneScore = "";//p1 score tracker
+	public static String playerTwoScore = "";//p2 score tracker
+	public static HashMap<String, String> gameBoard = new HashMap<String, String>();//track game board state
+	public static String topRowOutput = ("| ");//Player 2 output
+	public static String botRowOutput = ("| ");//Player 1 output
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int cnt = 1;
-		HashMap<String, String> gameBoard = new HashMap<String, String>();
-		do {
-			
+		initializeBoard();
+		//while game board top row or bottom row is not empty
+			//make move
+		printState();
+				
+	}
+	/*
+	 * Print the current state of the board with player scores
+	 * */
+	public static void printState() {
+		System.out.println(topRowOutput);
+		System.out.println(botRowOutput);
+		
+		playerOneScore += "Player One Score: " + playerOneScore; 
+		playerTwoScore += "Player Two Score: " + playerTwoScore;
+		
+		System.out.println(playerOneScore);
+		System.out.println(playerTwoScore);
+	}
+	/*
+	 * Initialize board with default bucket values
+	 * */
+	public static void initializeBoard() {
+		int cnt = 1;//counter to initialize buckets		
+		/*Fill out game board*/
+		do {			
 			gameBoard.put("a".concat(String.valueOf(cnt)), "4");
 			cnt++;
 		} while (cnt <= 6);
-		cnt = 1;
-		do {
-			
+		cnt = 1;//reset initializer
+		do {			
 			gameBoard.put("b".concat(String.valueOf(cnt)), "4");
 			cnt++;
 		} while (cnt <= 6);
-		String output = "";
-		for (int i = 1; i < 7; i++) {
-			if (mod(i,2)) {
-				System.out.print("| ".concat(gameBoard.get("a".concat(String.valueOf(i)))));
-			}
-			System.out.print(gameBoard.get("a".concat(String.valueOf(i))));
-//			output.concat("| ".concat(gameBoard.get("a".concat(String.valueOf(i)))));
-//			System.out.println(output);
+		//Print output
+		
+		for (int i = 0; i < 6; i++) {			
+			topRowOutput += (gameBoard.get("a".concat(String.valueOf(i+1))));
+			topRowOutput += (" | ");			
 		}
 		
-//		System.out.println(output);
-		
-		
+		for (int i = 0; i <6; i++) {				
+			botRowOutput += (gameBoard.get("b".concat(String.valueOf(i+1))));
+			botRowOutput += (" | ");		
+		}
 	}
-	public static boolean mod(int a, int b){
-	    if ( a < 0 ){
-	        return false;
-	    }else if (a==b){ 
-	        return true;
-	    }else{ 
-	        return mod( a-b, b );
-	    }
-	}
+
 	
 	
 
